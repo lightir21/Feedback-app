26544e62e22863b8e185a4cbc98b48ae3e92da4c
meir <lightir21@gmail.com>
2022-10-20 00:27:41 +0300

    Adding data and using it.
    -made a mock data folder with a file which includes an array of mock Feedbacks.
    -imported the data to app.js, set the data as the default value of our feedback state and sent as a prop to the a new component - FeedbackList
    -made a FeedbackList component and looped through each feedback and returned a FeedbackItem for each item in the array and sent down the item object as a prop.
    -used the data in FeedbackItem and displayed the feedback and its rating

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,20 +1,10 @@
 import { useState } from "react";
 
-const FeedbackItem = () => {
-  const [rating, setRating] = useState(7);
-  const [text, setText] = useState("this is an example");
-
-  const handleClick = () => {
-    setRating((prev) => {
-      return prev + 1;
-    });
-  };
-
+const FeedbackItem = ({ item }) => {
   return (
     <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Click</button>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
     </div>
   );
 };

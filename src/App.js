67cddf58d33bfece95f13f694ef1a416b8cc1967
meir <lightir21@gmail.com>
2022-10-21 01:15:40 +0300

    FeedbackForm
    -created FeedbackForm component
    -created a text form with a simple button
    -used onChage on input and set a function handleTextChange
    -handleTextChange function get a the event argument and then we set to the setText state the value (e.target.value)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
-
+import FeedbackForm from "./components/FeedbackForm";
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 
@@ -18,6 +18,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackForm />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>

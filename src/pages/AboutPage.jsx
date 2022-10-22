e0d44a6b9154d41df2b81df58d5c4bdf0048d927
meir <lightir21@gmail.com>
2022-10-23 00:38:48 +0300

    Link to About page and back to Home page & Create AboutIconLink component
    -create AboutIconLink.jsx component
    	AboutIcongLink.jsx:
    		-create a div with the class of about-link
    		-import Link from react router dom
    		-Link to '/about' set a question mark icon from font-awesome library inside the link   <Link to='/about'>icon</link>
    
    	App.js:
    		-impot and add AboutIconLink to the retun
    
    	AboutPage.jsx:
    		-import Link and use it to link back home '/'

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../components/shared/Card";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
@@ -8,7 +9,7 @@ const About = () => {
       <p>this is a React app to leave feedback for a product or service</p>
       <p>Version: 1.0.0</p>
       <p>
-        <a href="/">Back to home</a>
+        <Link to="/">Back to home</Link>
       </p>
     </Card>
   );

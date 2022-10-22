51b552ee00c5734306d9dc3f724d1cad4f5c4a3c
meir <lightir21@gmail.com>
2022-10-23 00:04:38 +0300

    Adding framer motion to animate the feedback items
    -npm install framer-motion
    
    	FeedbackList.jsx:
    		-import AnimatePresemce and motion
    		-wrap the mapping funtion with <AnimatePresence>
    		-wrap the feedbackItem with <motion.div>

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import PropTypes from "prop-types";
 import React from "react";
 import FeedbackItem from "./FeedbackItem";
@@ -9,11 +10,32 @@ const FeedbackList = ({ feedback, handleDelete }) => {
 
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // );
 };
 
 export default FeedbackList;

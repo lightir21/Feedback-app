68a60e7b9e5b3ca36c3c8f6ee4cf2700ec1264ef
meir <lightir21@gmail.com>
2022-10-25 02:46:42 +0300

    Add DeleteFeedback function into Context
    -in order to use our deleteFeedback function in every component and save on prop drilling we can just use it in the feedbackContext.
    
    App.js:
    	-cut out the deleteFeedback function
    	-delete the handleDelete prop that was passed down to FeedbackList
    
    FeedbackContext.js:
    	-paste the deleteFeedback function
    	-pass deleteFeedback function into the value of the provider in order to access it every where
    
    FeedbackList.jsx:
    	-delete the prop handle delete and delete in FeedbackItem the handle delete prop
    
    FeedbackItem.jsx:
    	-Delete handleDelete prop
    	-import useContext and FeedbackContext
    	-create a constant and destructure deleteFeedback from useContext hook and pass in FeedbackContext ->   const { deleteFeedback } = useContext(FeedbackContext);
    	-set inside the onclick function of the close button delteFeedback invoke it with the item.id parameter -> onClick={() => deleteFeedback(item.id)}

--- src/App.js
+++ src/App.js
@@ -18,12 +18,6 @@ function App() {
     setFeedback([newFeedback, ...feedback]);
   };
 
-  const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure you want to delete?")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
-    }
-  };
-
   return (
     <FeedbackProvider>
       <Router>
@@ -37,7 +31,7 @@ function App() {
                 <>
                   <FeedbackForm handleAdd={addFeedback} />
                   <FeedbackStats />
-                  <FeedbackList handleDelete={deleteFeedback} />
+                  <FeedbackList />
                 </>
               }
             ></Route>
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,17 @@
 import PropTypes from "prop-types";
 import { FaTimes } from "react-icons/fa";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Card from "./shared/Card";
+import FeedbackContext from "../context/FeedbackContext";
 
-const FeedbackItem = ({ item, handleDelete }) => {
+const FeedbackItem = ({ item }) => {
+  const { deleteFeedback } = useContext(FeedbackContext);
+
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
       <button className="close">
-        <FaTimes color="purple" onClick={() => handleDelete(item.id)} />
+        <FaTimes color="purple" onClick={() => deleteFeedback(item.id)} />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>

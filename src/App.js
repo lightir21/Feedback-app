29063e82f92d095128d0321d15c2fd3e8a2f78de
meir <lightir21@gmail.com>
2022-10-25 02:58:20 +0300

    Add feedback into context
    -in order to use addFeedback function every where in our app without prop drilling we set it inside the context
    
    App.js:
    	-cut addFeedback function and the import of uuid
    	-delete at FeedbackFrom the handleAdd prop
    
    FeedbackContext.js:
    	-paste addFeedback and the import of uuid
    	-add addFeedback into the provider value
    
    FeedbackForm.jsx:
    	-import useContext and FeedbackContext
    	-create a constant and destructure addFeedback from useContext hook and pass in FeedbackContext ->   const { addFeedback } = useContext(FeedbackContext);
    	-change handleAdd with addFeedback and set a parameter of newFeedback into it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import { v4 as uuidv4 } from "uuid";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import Header from "./components/Header";
-import FeedbackData from "./data/FeedbackData";
 import AboutPage from "./pages/AboutPage";
 import { FeedbackProvider } from "./context/FeedbackContext";
 import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
-  };
-
   return (
     <FeedbackProvider>
       <Router>
@@ -29,7 +19,7 @@ function App() {
               path="/"
               element={
                 <>
-                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackForm />
                   <FeedbackStats />
                   <FeedbackList />
                 </>
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
@@ -11,6 +12,11 @@ export const FeedbackProvider = ({ children }) => {
     },
   ]);
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
@@ -18,7 +24,7 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   return (
-    <FeedbackContext.Provider value={{ feedback, deleteFeedback }}>
+    <FeedbackContext.Provider value={{ feedback, deleteFeedback, addFeedback }}>
       {children}
     </FeedbackContext.Provider>
   );

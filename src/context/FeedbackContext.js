1602cfcceca83e5a388fd3bf20a45820445416dc
meir <lightir21@gmail.com>
2022-10-26 10:01:31 +0300

    adding the functionality of updateFeedback function
    FeedbackContext.js:
    	-inside updateFeedback insert setFeedback()
    
    	// inside of setFeedback you want to map through all the feedback items and search for the one that his id is equal to the id we pass through if it's equal then spread the feedback and the new updated feedback if not return the same feedback
    
    	-setFeedback(
          feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
        );

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -42,7 +42,9 @@ export const FeedbackProvider = ({ children }) => {
 
   // Update feedback item
   const updateFeedback = (id, updItem) => {
-    console.log(id, updItem);
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    );
   };
 
   // Set item to be updated
